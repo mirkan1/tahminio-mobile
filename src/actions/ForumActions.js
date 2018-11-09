bcc64f8be8df37c53f239b1b2862c521edffcff3
raq <mirkanbaba1@gmail.com>
2018-11-10 02:11:39 +0300

    Add: skeleton of actions

--- src/actions/ForumActions.js
+++ src/actions/ForumActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import _ from 'lodash';
-import { 
+import {
+	// MAKE_PREDICTION,
 } from './types';
 
 /*matchFetch
@@ -16,25 +17,101 @@ export const makePrediction = ({ match_id, text }) => {
 	// Endpoint `POST /v1/matches/:match_id/predictions/`
 	// `NOTE*: "game" is required and it must be from available games list`
 	// Response 201 and Prediction object
+	return (dispatch) => {
+    dispatch({ type: MAKE_PREDICTION });
+
+    axios.post(`http://api.tahmin.io/v1/matches/${match_id}/predictions/`, { 
+    	headers: 
+    	{ 
+	    	Authorization: `Token ${token}`
+				text: text,
+				game: match_id
+			}
+		})
+			.then(
+				// return to the same page
+			)
+			.catch(
+				// return to the same page with and error
+				error => console.log(error));
+  };
 };
 
 export const getListPrediction = ({ match_id }) => {
   // Description: Returns a list of Prediction objects of today
 	// Endpoint `GET /v1/matches/:match_id/predictions/`
 	// Response: 200 and list of Prediction objects
+
+	// `NOTE: If you want to get list of matches of another day, send a request like this: /v1/users/matches/?date=20-04-2018`
+
+	return (dispatch) => {
+    dispatch({ type: PREDICTION_LIST });
+
+    axios.get(`http://api.tahmin.io/v1/matches/${match_id}/predictions/`)
+			.then(() => {
+        // return to the desired page
+      })
+			.catch(err => console.log(err));
+  };
 };
 
-export const getPrediction = ({ match_id, prediction_id }) => {
+export const getPrediction = ({ match_id, prediction_id, TEXT, GAME }) => {
   // Description: Returns the requested prediction
 	// Endpoint `GET /v1/matches/:match_id/predictions/:prediction_id/`
 	// Response: 200 and a Prediction object
+	return (dispatch) => {
+    dispatch({ type: GET_PREDICTION });
+
+    axios.get(`http://api.tahmin.io/v1/matches/${match_id}/predictions/${prediction_id}/`, {
+    	headers: 
+    	{
+    		text: TEXT,
+    		game: GAME
+    	}
+    })
+			.then(() => {
+        // Done prediction and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
 };
 
-export const updatePrediction = ({ match_id, prediction_id }) => {
+export const deletePrediction = ({ match_id, prediction_id }) => {
+	// AUTH_REQ
+  // Description: Deletes the requested prediction
+	// Endpoint `DELETE /v1/matches/:match_id/predictions/:prediction_id/`
+	// Response: 204 and a Prediction object
+	return (dispatch) => {
+    dispatch({ type: DELETE_PREDICTION });
+
+    axios.delete(`http://api.tahmin.io/v1/matches/${match_id}/predictions/${prediction_id}/`)
+			.then(() => {
+        // Done deleting prediction and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
+};
+
+export const updatePrediction = ({ match_id, prediction_id, TEXT, GAME }) => {
 	// AUTH_REQ
   // Description: Updates the requested prediction
 	// Endpoint `PATCH /v1/matches/:match_id/predictions/:prediction_id/`
 	// Response: 200 and a Prediction object
+	return (dispatch) => {
+    dispatch({ type: UPDATE_PREDICTION });
+
+    axios.patch(`http://api.tahmin.io/v1/matches/${match_id}/predictions/${prediction_id}/`, {
+    	headers: 
+    	{
+    		text: TEXT,
+    		game: GAME
+    	}
+    })
+			.then(() => {
+        // Done updating the prediction and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const postMessageToMatch = ({ match_id, text }) => {
@@ -42,6 +119,20 @@ export const postMessageToMatch = ({ match_id, text }) => {
 	// Description: Posts a message to the match's thread
 	// Endpoint `POST /v1/matches/:match_id/messages/`
 	// Response: 201 and a message object
+	return (dispatch) => {
+    dispatch({ type: POST_MESSAGE_TO_MATCH });
+
+    axios.post(`http://api.tahmin.io/v1/matches/${match_id}/messages/`, {
+    	headers: 
+    	{
+    		text: TEXT,
+    	}
+    })
+			.then(() => {
+        // Done message and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const getListOfMessages = () => {
@@ -49,6 +140,16 @@ export const getListOfMessages = () => {
   // Description: Returns a list of Prediction objects of today
 	// Endpoint `GET /v1/matches/:match_id/messages/`
 	// Response: 200 and list of Message objects
+	return (dispatch) => {
+    dispatch({ type: GET_LIST_OF_MESSAGES });
+
+    axios.get(`http://api.tahmin.io/v1/matches/${match_id}/messages/`)
+			.then((messages) => {
+				console.log(messages)
+        // Render to message list
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const updateMessage = ({ match_id, message_id, new_message }) => {
@@ -56,24 +157,81 @@ export const updateMessage = ({ match_id, message_id, new_message }) => {
   // Description: Updates the given message to the match's thread
 	// Endpoint `PATCH /v1/matches/:match_id/messages/:message_id/`
 	// Response: 200 and a message object
+	return (dispatch) => {
+    dispatch({ type: UPDATE_MESSAGE });
+
+    axios.patch(`http://api.tahmin.io/v1/matches/${match_id}/messages/${message_id}}`, {
+    	headers: 
+    	{
+    		text: new_message,
+    	}
+    })
+			.then(() => {
+        // Done message updating and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
+};
+
+export const deleteMessage = ({ match_id, message_id }) => {
+  // Description: Deletes the given message
+	// Endpoint `DELETE /v1/matches/:match_id/messages/:message_id/`
+	// Response: 204
+	return (dispatch) => {
+    dispatch({ type: DELETE_MESSAGE });
+
+    axios.delete(`http://api.tahmin.io/v1/matches/${match_id}/messages/${message_id}/`)
+			.then(() => {
+        // Render to message list
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const getMessage = ({ match_id, message_id }) => {
   // Description: Returns the requested message
 	// Endpoint `GET /v1/matches/:match_id/messages/:message_id/`
 	// Response: 200 and a Message object
+	return (dispatch) => {
+    dispatch({ type: GET_MESSAGE });
+
+    axios.get(`http://api.tahmin.io/v1/matches/${match_id}/messages/${message_id}/`)
+			.then((message) => {
+        console.log(message)
+        // Render to message list
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const upvotePrediction = ({ match_id, prediction_id }) => {
   // Description: Upvotes the given prediction
 	// Endpoint `POST /v1/matches/:match_id/predictions/:prediction_id/upvote/`
 	// Response: 200
+	return (dispatch) => {
+    dispatch({ type: UPVOTE_PREDICTION });
+
+    axios.post(`http://api.tahmin.io/v1/matches/${match_id}/predictions/${prediction_id}/upvote`)
+			.then(() => {
+        // upvote the message and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const undoUptovePrediction = ({ match_id, prediction_id }) => {
   // Description: Undo upvotes the given prediction
 	// Endpoint `POST /v1/matches/:match_id/predictions/:prediction_id/undoupvote/`
 	// Response: 200
+	return (dispatch) => {
+    dispatch({ type: UNDO_UPVOTE_PREDICTION });
+
+    axios.post(`http://api.tahmin.io/v1/matches/${match_id}/predictions/${prediction_id}/undoupvote`)
+			.then(() => {
+        // undo the upvote the message and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const getAMatch = ({ match_id }) => {
@@ -84,10 +242,29 @@ export const getAvailableGames = ({ match_id }) => {
   // Description: Returns the available game types for prediction
 	// Endpoint `GET /v1/matches/:match_id/games/`
 	// Response 200 and list of Game objects
+	return (dispatch) => {
+    dispatch({ type: GET_AVAILABLE_GAMES });
+
+    axios.post(`http://api.tahmin.io/v1/matches/${match_id}/games/`)
+			.then((games) => {
+				console.log(games)
+        // undo the upvote the message and render to the same page
+      })
+			.catch(err => console.log(err));
+  };
 };
 
 export const getMatchlistMeta = () => {
   // Description: Returns the metadata of the matchlist. For now it only contains dates.
 	// Endpoint `GET /v1/matches/meta/`
 	// Response 200 and
+	return (dispatch) => {
+    dispatch({ type: GET_MATCHLIST_META });
+
+    axios.post(`http://api.tahmin.io/v1/matches/meta`)
+			.then(() => {
+				// render to the meta page
+      })
+			.catch(err => console.log(err));
+  };
 };
--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -3,64 +3,218 @@ import _ from 'lodash';
 import { 
 } from './types';
 
-export const userSignUp = ({ username, passport, email, first_name=null, last_name=null, bio=null }) => {
+export const userSignUp = ({ username, passpord, email, first_name=null, last_name=null, bio=null }) => {
   // Description: Creates a new user with given data
   // Endpoint `POST /v1/users/signup/`
+  return (dispatch) => {
+    dispatch({ type: USER_SIGN_UP });
+
+    axios.post(`http://api.tahmin.io/v1/users/signup/`, { 
+      headers: 
+      { 
+        "username": username,
+        "password": password,
+        "email": email,
+        "first_name": first_name,
+        "last_name": last_name,
+        "bio": bio
+      }
+    })
+      .then(user => {
+        console.log(user)
+        // render to the user profile page
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
 export const userLogin = () => {
   // Already done in .AuthActions.js
 };
 
-export const userGetMe = () => {
+export const userGetMe = ({ token }) => {
   // Description: Returns the information about authed user (current user)
   // Endpoint `GET /v1/users/me/`
   // Response: 200 and UserMe object
+  return (dispatch) => {
+    dispatch({ type: USER_GET_ME });
+
+    axios.get('http://api.tahmin.io/v1/users/me/', {
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+      }
+    })
+      .then(user => {
+        console.log(user);
+      })
+      .catch((err) => console.log(err));
+  };
 };
 
-export const userDeleteMe = () => {
+export const userDeleteMe = ({ token }) => {
   // Description: Deletes the current user
   // Endpoint `DELETE /v1/users/me/`
   // Response: 204
+  return (dispatch) => {
+    dispatch({ type: USER_DELETE_ME });
+
+    axios.delete(`http://api.tahmin.io/v1/users/me/`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+      }
+    })
+      .then(user => {
+        LoginUserSuccess(dispatch, user);
+      })
+      .catch(() => LoginUserFail(dispatch));
+  };
 };
 
-export const userUpdateMe = ({ username, passport, email, first_name=null, last_name=null, bio=null , profile_photo=null }) => {
+export const userUpdateMe = ({ username, passport, email, first_name=null, last_name=null, bio=null , profile_photo=null, token }) => {
   // Note: You don't need to send every field. Sending the changing fields is enough.
   // Endpoint `PATCH /v1/users/me/`
   // Response: 200 and UserMe object
+  return (dispatch) => {
+    dispatch({ type: USER_SIGN_UP });
+
+    axios.patch(`http://api.tahmin.io/v1/users/me/`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+        "username": username,
+        "password": password,
+        "email": email,
+        "first_name": first_name,
+        "last_name": last_name,
+        "bio": bio,
+        "profile_photo": profile_photo
+      }
+    })
+      .then(user => {
+        console.log(user)
+        // render to the user profile page
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const getAnotherUser = ({ user_id }) => {
+export const getAnotherUser = ({ user_id, token }) => {
   // Description: Returns the information about requested user
   // Endpoint `GET /v1/users/:user_id/`
   // Return: 200 and UserDetail object
+  return (dispatch) => {
+    dispatch({ type: GET_ANOTHER_USER });
+
+    axios.get(`http://api.tahmin.io/v1/users/${user_id}/`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+      }
+    })
+      .then(user_detail => {
+        console.log(user_detail)
+        // render to the user profile page
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const userChangePassword = ({ old_password, new_password }) => {
+export const userChangePassword = ({ old_password, new_password, token }) => {
   // Description: Changes the password of authed user
   // Endpoint `PATCH /v1/users/me/password/`
   // Return: 200 and UserMe object
+  return (dispatch) => {
+    dispatch({ type: USER_CHANGE_PASSWORD });
+
+    axios.patch(`http://api.tahmin.io/v1/users/me/password/`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`,
+        "old_password": old_password,
+        "new_password": new_password
+      }
+    })
+      .then(user_detail => {
+        console.log(user_detail)
+        // render to the user profile page
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const followUser = ({ user_id }) => {
+export const followUser = ({ user_id, token }) => {
   // Description: Follows the given user
   // Endpoint `POST /v1/users/:user_id/follow/`
   // Return: 200
+  return (dispatch) => {
+    dispatch({ type: FOLLOW_USER });
+
+    axios.post(`http://api.tahmin.io/v1/users/${user_id}/follow/`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+      }
+    })
+      .then(() => {
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const unfollowUser = ({ user_id }) => {
+export const unfollowUser = ({ user_id, token }) => {
   // Description: Unfollows the given user
   // Endpoint `POST /v1/users/:user_id/unfollow/`
   // Return: 200
+  return (dispatch) => {
+    dispatch({ type: UNFOLLOW_USER });
+
+    axios.post(`http://api.tahmin.io/v1/users/${user_id}/unfollow/`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+      }
+    })
+      .then(() => {
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
 export const userVerify = ({ verification_key }) => {
   // Description: Verifies the user associated with the given code
   // Endpoint `GET /v1/users/activate/?key=:verification_key`
   // Return: 200
+  return (dispatch) => {
+    dispatch({ type: USER_VERIFY });
+
+    axios.get(`http://api.tahmin.io/v1/users/activate/?key=:${verification_key}`
+      .then(() => {
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
 export const userSearch = ({ your_query }) => {
+  // DONE ON AuthActions.js
   // Description: Searchs the user by username
   // Endpoint `GET /v1/search/users/?query=:your_query`
   // Return 200 and list of SimpleUser objects
@@ -76,42 +230,146 @@ export const requestPasswordReset = ({ user_identifier }) => {
   // After this step, an email will be send to the user, containing a link like this:
   // `tahmin.io/change-password/?key=ABCDE1234567`
   // You will use the key in the url to change the user's password
+  return (dispatch) => {
+    dispatch({ type: REQUEST_PASSWORD_RESET });
+
+    axios.post(`http://api.tahmin.io/v1/users/forgot_password/`, { 
+      headers: 
+      { 
+        user_identifier: user_identifier
+      }
+    })
+      .then(() => {
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const ChangePasswordWithKey = ({ key, password }) => {
+export const changePasswordWithKey = ({ key, password }) => {
   // Endpoint `POST /v1/users/change_password/`
   // Response:
   // 200 for successfull password change
   // 404 for invalid key
   // 400 for invalid password
+  // `tahmin.io/change-password/?key=ABCDE1234567`
+  return (dispatch) => {
+    dispatch({ type: CHANGE_PASSWORD_WITH_KEY });
+
+    axios.post(`http://api.tahmin.io/v1/users/change_password/`, { 
+      headers: 
+      { 
+        key: key,
+        password: password
+      }
+    })
+      .then(() => {
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const getMinetTrophies = () => {
+export const getUserTrophies = () => {
   // Endpoint `GET /v1/users/me/trophies/`
   // Response: 200 and list of Trophy objects
+  return (dispatch) => {
+    dispatch({ type: GET_USER_TROPHIES });
+
+    axios.get(`http://api.tahmin.io/v1/users/me/trophies`)
+      .then((trophies) => {
+        console.log(trophies)
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const getUserTrophies = ({ user_id }) => {
+export const getAnotherUserTrophies = ({ user_id }) => {
   // Endpoint `GET /v1/users/:user_id/trophies/`
   // Response: 200 and list of Trophy objects
+  return (dispatch) => {
+    dispatch({ type: GET_ANOTHER_USER_TROPHIES });
+
+    axios.get(`http://api.tahmin.io/v1/users/${user_id}/trophies`)
+      .then((trophies) => {
+        console.log(trophies)
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const allTimeLeaderboard = ({ page_number="total_pages" }) => {
+export const getAllTimeLeaderboard = ({ page_number="total_pages" }) => {
   // Endpoint `GET /v1/leaders/:page_number/`
   // `For total page number send a request to /v1/leaders/total_pages/`
   // Response 200 and list of SimpleUser objects
+  return (dispatch) => {
+    dispatch({ type: GET_ALL_TIME_LEADERBOARD });
+
+    axios.get(`http://api.tahmin.io/v1/leaders/${page_number}/`)
+      .then((SimpleUser) => {
+        console.log(SimpleUser)
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
-export const getMineProgress = () => {
-  // `Authentication token is required in this endpoint`
+export const getUserTrophyProgress = () => {
+  // AUTH_REQ
   // Endpoint `GET /v1/users/me/progression/`
   // Response: 200 and list of TrophyProgression objects
+  return (dispatch) => {
+    dispatch({ type: GET_USER_TROPHY_PROGRESS });
+
+    axios.get(`http://api.tahmin.io/v1/uesrs/me/progression`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+      }
+    })
+      .then((TrophyProgression) => {
+        console.log(TrophyProgression)
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
 export const getUserFeed = () => {
-  // `Authentication token is required in this endpoint`
+  // AUTH_REQ
   // Endpoint `GET /v1/users/feed/`
   // Response: 200 and list of Prediction objects
+  return (dispatch) => {
+    dispatch({ type: GET_USER_FEED });
+
+    axios.get(`http://api.tahmin.io/v1/uesrs/feed/`, { 
+      headers: 
+      { 
+        Authorization: `Token ${token}`
+      }
+    })
+      .then((predictions) => {
+        console.log(predictions)
+        // render to the user profile
+      })
+      .catch(
+        // return to the same page with and error
+        error => console.log(error));
+  };
 };
 
 
